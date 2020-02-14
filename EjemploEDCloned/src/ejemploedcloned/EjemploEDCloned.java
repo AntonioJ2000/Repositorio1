4702f85377f69a9a66b942169d96f0abb29e68f9
anton <anton@LAPTOP-25P7PRFR>
2020-02-14 13:32:23 +0100

    prueba

---
+++ EjemploEDCloned/src/ejemploedcloned/EjemploEDCloned.java
@@ -0,0 +1,20 @@
+package ejemploedcloned;
+
+import java.util.Scanner;
+
+public class EjemploEDCloned {
+
+    public static void main(String[] args) {
+       Scanner teclado = new Scanner(System.in);
+       String cad1;
+       String cad2;
+       System.out.println("Introduce una frase");
+       cad1=teclado.nextLine();
+       System.out.println("Introduce otra frase");
+       cad2=teclado.nextLine();
+       
+       System.out.println("cad1 tiene "+ cad1.length()+ " caracteres");
+       System.out.println("cad2 tiene "+ cad2.length()+ " caracteres");
+    }
+    
+}
